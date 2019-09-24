fa10fa09f738346356cb3e08d6a86eed4e4e1c59
Pavel Kazantsev <pv.kazantsev@gmail.com>
2019-09-24 19:14:39 +0300

    Initial Commit

---
+++ SimBatchDelete/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SimBatchDelete
+//
+//  Created by p.kazantsev on 24/09/2019.
+//  Copyright © 2019 pakaz.net. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SimBatchDelete/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SimBatchDelete
+//
+//  Created by p.kazantsev on 24/09/2019.
+//  Copyright © 2019 pakaz.net. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
