c15936db9e6b00ccece3d804a269fb9249fcf7e4
Pavel Kazantsev <pv.kazantsev@gmail.com>
2019-10-08 23:02:47 +0300

    Extracted common code in SimctlCommand

--- SimBatchDelete/SimctlCommand.swift
+++ SimBatchDelete/SimctlCommand.swift
@@ -56,68 +56,31 @@ class SimCtlCommand {
 
     func run(then completion: @escaping (Result<Simulators, ParserError>) -> Void) {
 
-        DispatchQueue.global(qos: .background).async {
-            let cmd = Process()
-            cmd.launchPath = "/usr/bin/xcrun"
-            cmd.arguments = [
-                "simctl",
-                "list",
-                "-j"
-            ]
-
-            let resultHandler = Pipe()
-            cmd.standardOutput = resultHandler
-            cmd.terminationHandler = { _ in
-                let data = resultHandler.fileHandleForReading.readDataToEndOfFile()
-                let decoder = JSONDecoder()
-                do {
-                    let sims = try decoder.decode(Simulators.self, from: data)
-                    completion(.success(sims))
-                }
-                catch {
-                    completion(.failure(.jsonDecodeError(error)))
-                }
+        execute(path: "/usr/bin/xcrun", args: ["simctl", "list", "-j"]) { data in
+            let decoder = JSONDecoder()
+            do {
+                let sims = try decoder.decode(Simulators.self, from: data)
+                completion(.success(sims))
             }
-
-            cmd.launch()
-            cmd.waitUntilExit()
-
-            print("Execution status: \(cmd.terminationStatus)")
+            catch {
+                completion(.failure(.jsonDecodeError(error)))
+            }
         }
     }
 
     func deleteDevice(_ identifier: UUID, then completion: @escaping (Result<Void, ParserError>) -> Void) {
 
-        DispatchQueue.global(qos: .background).async {
-            let cmd = Process()
-            cmd.launchPath = "/usr/bin/xcrun"
-            cmd.arguments = [
-                "simctl",
-                "delete",
-                identifier.uuidString
-            ]
-
-            let resultHandler = Pipe()
-            cmd.standardOutput = resultHandler
-            cmd.terminationHandler = { _ in
-                let data = resultHandler.fileHandleForReading.readDataToEndOfFile()
-                if data.count > 0 {
-                    if let errorStr = String(data: data, encoding: .utf8) {
-                        completion(.failure(.unexpectedResult(errorStr)))
-                    }
-                    else {
-                        completion(.failure(.emptyResult))
-                    }
-                }
-                else {
-                    completion(.success(()))
-                }
+        execute(path: "/usr/bin/xcrun", args: ["simctl", "delete", identifier.uuidString]) { data in
+            guard data.count > 0 else {
+                completion(.success(()))
+                return
             }
-
-            cmd.launch()
-            cmd.waitUntilExit()
-
-            print("Execution status: \(cmd.terminationStatus)")
+            if let errorStr = String(data: data, encoding: .utf8) {
+                completion(.failure(.unexpectedResult(errorStr)))
+            }
+            else {
+                completion(.failure(.emptyResult))
+            }
         }
     }
 
@@ -125,39 +88,40 @@ class SimCtlCommand {
 
     func fetchToolchainVersion(then completion: @escaping (Result<Toolchain, ParserError>) -> Void) {
 
+        execute(path: "/usr/bin/xcodebuild", args: ["-version"]) { data in
+            guard let versionStr = String(data: data, encoding: .utf8) else {
+                completion(.failure(.emptyResult))
+                return
+            }
+
+            let parts = versionStr.split(separator: "\n")
+            guard parts.count == 2 else {
+                completion(.failure(.unexpectedResult(versionStr)))
+                return
+            }
+
+            let xcVersion = String(parts[0])
+            let buildVersion = String(parts[1].split(separator: " ").last ?? "")
+
+            completion(.success(Toolchain(xcodeVersion: xcVersion, xcodeBuildVersion: buildVersion)))
+        }
+    }
+
+    private func execute(path: String, args: [String], then completion: @escaping (Data) -> Void) {
+
         DispatchQueue.global(qos: .background).async {
             let cmd = Process()
-            cmd.launchPath = "/usr/bin/xcodebuild"
-            cmd.arguments = [
-                "-version"
-            ]
+            cmd.launchPath = path
+            cmd.arguments = args
 
             let resultHandler = Pipe()
             cmd.standardOutput = resultHandler
             cmd.terminationHandler = { _ in
                 let data = resultHandler.fileHandleForReading.readDataToEndOfFile()
-
-                guard let versionStr = String(data: data, encoding: .utf8) else {
-                    completion(.failure(.emptyResult))
-                    return
-                }
-
-                let parts = versionStr.split(separator: "\n")
-                guard parts.count == 2 else {
-                    completion(.failure(.unexpectedResult(versionStr)))
-                    return
-                }
-
-                let xcVersion = String(parts[0])
-                let buildVersion = String(parts[1].split(separator: " ").last ?? "")
-
-                completion(.success(Toolchain(xcodeVersion: xcVersion, xcodeBuildVersion: buildVersion)))
+                completion(data)
             }
 
             cmd.launch()
-            cmd.waitUntilExit()
-
-            print("Execution status: \(cmd.terminationStatus)")
         }
     }
 }
